14749c03c475230a2b0ffde1983a3f8269adb08b
JustinKubicek <JustinKubicek@Gmail.com>
2019-06-10 14:16:16 -0500

    Commit

---
+++ javascript-example.js
@@ -0,0 +1,12 @@
+var lawntools = [
+	'Mower',
+	'Weed whip',
+	'Shovel',
+	'Garden Hoe'
+];
+
+lawntools.forEach(function (tool, index) {
+	console.log(index + ": " + tool);
+});
+
+// returns "0: Mower", "1: Weed whip", "2: Shovel", "3: "Garden Hoe"
