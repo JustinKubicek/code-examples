858b85f3ec69f53c0822e1a3c121aab9c13b4242
JustinKubicek <JustinKubicek@Gmail.com>
2019-06-10 14:28:14 -0500

    update javascript examples

--- javascript-example.js
+++ javascript-example.js
@@ -1,3 +1,5 @@
+//Example 1 For Each
+
 var lawntools = [
 	'Mower',
 	'Weed whip',
@@ -10,3 +12,18 @@ lawntools.forEach(function (tool, index) {
 });
 
 // returns "0: Mower", "1: Weed whip", "2: Shovel", "3: "Garden Hoe"
+
+// Other examples of selecting without using jQuery
+// jQuery version $("#example")
+// Vanilla version:
+document.getElementById('example')
+// jQuery version $(".example")
+// Vanilla version:
+document.getElementsByClassName('.example')
+
+// Fade out an element, then remove it. 
+var eObj = document.getElementById('section__5').style;
+eObj.opacity = 1;
+(function fade(){
+    (eObj.opacity-=.1) < 0 ? eObj.display="none" : setTimeout(fade,40)
+})();
